2fe0f2a56547761a2e37e8386e2b57395f874679
opsylon <69088845+opsylon@users.noreply.github.com>
2020-12-10 22:16:11 -0500

    fix bugs
    -fixed building math
    -made page title only update every 5 sec and on data load.

--- expiriments/pretzel/main.js
+++ expiriments/pretzel/main.js
@@ -1,10 +1,26 @@
 var pretzelsStored = 0;
 var clickerAmount = 0;
+var build1Check = false;
+var build1persec = true;
 
-window.onload = autoSave();
-function autoSave() {
+window.onload = processStart();
+function processStart() {
     load();
     setInterval(save, 60000);
+    setInterval(updateprzlcnt, 5000);
+    if (clickerAmount < 10 && build1Check === false) {
+        setInterval(Clicker, 10000);
+        build1Check = true
+        build1persec = false
+    };
+    if (clickerAmount >= 10 && build1Check === false || build1persec === false) {
+        if (build1persec === false) {
+            clearInterval(Clicker, 10000);
+            build1persec = true;
+        };
+        setInterval(Clicker, 1000);
+        build1Check = true;
+    };
 };
 function save() {
     localStorage.setItem('pretzelcount', pretzelsStored);
@@ -18,10 +34,10 @@ function load() {
     clickerAmount = parseInt(clickerAmount);
     document.title = pretzelsStored + ' Pretzels';
     document.getElementById('prtzlcount').innerHTML = 'Pretzels: ' + pretzelsStored;
+    document.getElementById('clicker').innerHTML = 'Clicker! ' + clickerAmount + ' Owned'
 };
 function pretzelClicked() {
     pretzelsStored = pretzelsStored + 1;
-    document.title = pretzelsStored + ' Pretzels';
     document.getElementById('prtzlcount').innerHTML = 'Pretzels: ' + pretzelsStored;
 };
 function buyClicker() {
@@ -30,13 +46,31 @@ function buyClicker() {
         return alert('You do not have enough Pretzels to buy.');
     };
     document.getElementById('clicker').innerHTML = 'Clicker! ' + clickerAmount + ' Owned'
-    if (clickerAmount > 0) {
+    if (clickerAmount < 10 && build1Check === false) {
         setInterval(Clicker, 10000);
+        build1Check = true;
+        build1persec = false;
+    };
+    if (clickerAmount >= 10 && build1Check === false || build1persec === false) {
+        if (build1persec === false) {
+            clearInterval(Clicker, 10000);
+            build1persec = true;
+        };
+        setInterval(Clicker, 1000);
+        build1Check = true;
     };
 };
 function Clicker() {
-    var clickerEarnings = clickerAmount * 1
+    var clickerEarnings = .1
+    if (clickerAmount < 10) {
+        clickerEarnings = clickerAmount * clickerEarnings * 1;
+    };
+    if (clickerAmount >= 10) {
+        clickerEarnings = clickerAmount * clickerEarnings / 10 * 1;
+    }
     pretzelsStored = pretzelsStored + clickerEarnings;
-    document.title = pretzelsStored + ' Pretzels'
     document.getElementById('prtzlcount').innerHTML = 'Pretzels: ' + pretzelsStored;
 };
+function updateprzlcnt() {
+    document.title = pretzelsStored + ' Pretzels';
+}
