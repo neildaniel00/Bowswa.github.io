ddd60461cc247f433681730782e6729441b24f3e
Bowswa <69088845+Bowswa@users.noreply.github.com>
2021-03-24 12:28:14 -0400

    fix more bugs

--- experiments/happy-birthday-machine/birthday.js
+++ experiments/happy-birthday-machine/birthday.js
@@ -1,10 +1,10 @@
 function webLoad() {
     var bDayName = localStorage.getItem('BdayPerson');
     document.title = 'Happy Birthday ' + bDayName + '!';
-    document.getElementById('BDayName').innerHTML = bDayName
-}
+    document.getElementById('BDayName').innerHTML = bDayName;
+};
 function resetName() {
     localStorage.removeItem('BdayPerson')
     location.replace('https://bowswa.github.io/experiments/happy-birthday-machine/index.html');
-}
+};
 window.onload = webLoad();
