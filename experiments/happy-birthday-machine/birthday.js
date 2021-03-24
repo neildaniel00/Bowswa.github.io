820e00d45157b71208c254991d3a29616acfcda1
Bowswa <69088845+Bowswa@users.noreply.github.com>
2021-03-24 12:08:34 -0400

    push workaround stuff

--- experiments/happy-birthday-machine/birthday.js
+++ experiments/happy-birthday-machine/birthday.js
@@ -3,4 +3,8 @@ function webLoad() {
     document.title = 'Happy Birthday ' + bName + '!'
     document.getElementById('bName').innerHTML = bName
 }
+function resetName() {
+    localStorage.removeItem('BdayPerson')
+    location.replace('https://bowswa.github.io/experiments/happy-birthday-machine/index.html');
+}
 window.onload = webLoad();
