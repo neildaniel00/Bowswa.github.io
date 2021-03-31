de3f6ef27590f0c58229d34d4baf529f19ac2a2e
Bowswa <69088845+Bowswa@users.noreply.github.com>
2021-03-31 17:36:31 -0400

    re-do feature differently

--- af.js
+++ af.js
@@ -4,20 +4,9 @@ function checkPass() {
     localStorage.setItem('RRPassed', true);
     location.replace('https://bowswa.github.io/index.html');
   }else {
-    return;
+    redirect();
   }
 };
-function redirectOne() {
-    localStorage.setItem('COne', true);
+function redirect() {
     location.replace('https://www.youtube.com/watch?v=d1YBv2mWll0');
-}
-function winLoad() {
-  var check1 = localStorage.getItem('COne');
-  var element = document.getElementsByClassName('one');
-  if (check1 = true) {
-    element.style.display = 'none';
-  }else {
-    return;
-  }
-}
-window.onload = winLoad();
+};
